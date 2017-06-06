08f6018cf7317b218e8f5896fde152011801c825
Biddrup Kumar Mallick <Biddrup@users.noreply.github.com>
2017-06-06 08:56:18 -0700

    am66

--- functions.php
+++ functions.php
@@ -1,32 +1,52 @@
 <?php
-	//The begenning of theme support area
-	function htw_theme_support(){
+	function am66_theme_support(){
 		add_theme_support('title-tag');
+		register_nav_menus(array(
+			'primary_menu' => 'Primery Menu',
+		));
 	}
-	add_action('after_setup_theme','htw_theme_support');
+	add_action('after_setup_theme', 'am66_theme_support');
 
-	//The begenning of script and style
-	function htw_scripts(){
-		wp_enqueue_style('google-fonts-one', '//fonts.googleapis.com/css?family=Open+Sans:400,300', null, 'v1.0', all);
-		wp_enqueue_style('google-fonts-two', '//fonts.googleapis.com/css?family=PT+Sans', null, 'v1.0', all);
-		wp_enqueue_style('google-fonts-three', '//fonts.googleapis.com/css?family=Raleway', null, 'v1.0', all);
+	function am66_css_js(){
+		wp_enqueue_style('google-font-opensans', '//fonts.googleapis.com/css?family=Open+Sans:400,300');
+		wp_enqueue_style('google-font-ptsans', '//fonts.googleapis.com/css?family=PT+Sans');
+		wp_enqueue_style('google-font-raleway', '//fonts.googleapis.com/css?family=Raleway');
 		wp_enqueue_style('bootstrap', get_template_directory_uri().'/assets/bootstrap/css/bootstrap.css', null);
-		wp_enqueue_style('font-awesome.min', get_template_directory_uri().'/assets/css/font-awesome.min.css', null);
-		wp_enqueue_style('style', get_template_directory_uri().'/assets/css/style.css', null);
+		wp_enqueue_style('font-awesome', get_template_directory_uri().'/assets/css/font-awesome.min.css', null);
+		wp_enqueue_style('style.css', get_template_directory_uri().'/assets/css/style.css', null, 'v1.0', all);
 		wp_enqueue_style('animate.min', get_template_directory_uri().'/assets/css/animate.min.css', null);
-		wp_enqueue_style('main-css', get_stylesheet_uri(), null, 'v1.0', all);
+		wp_enqueue_style('style-projects', get_template_directory_uri().'/assets/css/style-projects.css', null, 'v1.0', all);
+		wp_enqueue_style('mother-style', get_stylesheet_uri());
 
-		//The begenning of script
 		wp_enqueue_script('jquery');
-		wp_enqueue_script('bootstrap', get_template_directory_uri().'/assets/bootstrap/js/bootstrap.min.js', 'jquery', null, true);
-		wp_enqueue_script('wow', get_template_directory_uri().'/js/wow.min.js', 'jquery', null, true);
+		wp_enqueue_script('bootstrap.min.js', get_template_directory_uri().'/assets/bootstrap/js/bootstrap.min.js', 'jquery', 'v3.0', true);
+		wp_enqueue_script('wow.min.js', get_template_directory_uri().'/js/wow.min.js', 'jquery', 'v3.0', true);
 	}
-	add_action('wp_enqueue_scripts','htw_scripts');
+	add_action('wp_enqueue_scripts', 'am66_css_js');
 
-	function footer_extra_script(){
-		?>
+	function footer_extra_script(){ ?>
 		<script>
 		  new WOW().init();
 		</script>
-<?php	}
-	add_action('wp_footer', 'footer_extra_script', 30);
+	<?php }
+	add_action('wp_footer', 'footer_extra_script', 21);
+
+	function am66_fallback_menu(){ ?>
+		<ul class="nav navbar-nav pull-right">
+            <li class="active">
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">About</a>
+            </li>
+            <li>
+                <a href="#">Blog</a>
+            </li>
+            <li>
+                <a href="#">Team</a>
+            </li>
+            <li>
+                <a href="#"><span>Contact</span></a>
+            </li>
+        </ul>	
+	<?php	}
--- index.php
+++ index.php
@@ -1,32 +1,32 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
+    <head>
 
-<head>
+        <!-- meta data & title -->
+        <meta charset="<?php bloginfo('charset') ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-    <!-- meta data & title -->
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+        <!-- CSS -->
+        
 
-    <!-- CSS -->
-	<!-- They are called by functions.php -->
+        <!-- Favicon and touch icons -->
+        <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/ico/favicon.ico">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/assets/ico/apple-touch-icon-144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/assets/ico/apple-touch-icon-114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/assets/ico/apple-touch-icon-72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri(); ?>/assets/ico/apple-touch-icon-57-precomposed.png">
+        <?php wp_head(); ?>
 
-    <!-- Favicon and touch icons -->
-    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/assets/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/assets/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/assets/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri(); ?>/assets/ico/apple-touch-icon-57-precomposed.png">
-    <?php wp_head(); ?>
-</head>
-
-<body>
+    </head>
+  <body>
 
     <!-- Header -->
+        
     <nav id="navbar-section" class="navbar navbar-default navbar-static-top navbar-sticky" role="navigation">
         <div class="container">
-
+        
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
                     <span class="sr-only">Toggle navigation</span>
@@ -35,11 +35,19 @@
                     <span class="icon-bar"></span>
                 </button>
 
-                <a class="navbar-brand wow fadeInDownBig" href="index.html"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/slider/Office.png" width="100" alt="Office"></a>
+                <a class="navbar-brand wow fadeInDownBig" href="<?php echo site_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/slider/Office.png" width="100" alt="Office"></a>      
             </div>
-
+        
             <div id="navbar-spy" class="collapse navbar-collapse navbar-responsive-collapse">
-                <ul class="nav navbar-nav pull-right">
+
+            <?php
+                wp_nav_menu(array(
+                    'theme_location'=>'primary_menu',
+                    'fallback_cb'=>'am66_fallback_menu',
+                    'container'=>'nav',
+                ))
+            ?>
+                <!-- <ul class="nav navbar-nav pull-right">
                     <li class="active">
                         <a href="index.html">Home</a>
                     </li>
@@ -55,15 +63,16 @@
                     <li>
                         <a href="contact.html"><span>Contact</span></a>
                     </li>
-                </ul>
+                </ul>  -->        
             </div>
         </div>
     </nav>
+
     <!-- End Header -->
 
 
     <!-- Begin #carousel-section -->
-    <section id="carousel-section" class="section-global-wrapper">
+    <section id="carousel-section" class="section-global-wrapper"> 
         <div class="container-fluid-kamn">
             <div class="row">
                 <div id="carousel-1" class="carousel slide">
@@ -74,7 +83,7 @@
                         <li data-target="#carousel-1" data-slide-to="1"></li>
                         <li data-target="#carousel-1" data-slide-to="2"></li>
                     </ol>
-
+        
                     <!-- Wrapper for slides -->
                     <div class="carousel-inner">
                         <!-- Begin Slide 1 -->
@@ -107,7 +116,7 @@
                         </div>
                         <!-- End Slide 3 -->
                     </div>
-
+        
                     <!-- Controls -->
                     <a class="left carousel-control" href="#carousel-1" data-slide="prev">
                         <span class="glyphicon glyphicon-chevron-left"></span>
@@ -128,11 +137,10 @@
             <div class="row">
                 <div class="services-header">
                     <h3 class="services-header-title">Our Mission</h3>
-                    <p class="services-header-body"><em> Things we provide in Office </em> </p>
-                    <hr>
+                    <p class="services-header-body"><em> Things we provide in Office </em>  </p><hr>
                 </div>
             </div>
-
+      
             <!-- Begin Services Row 1 -->
             <div class="row services-row services-row-head services-row-1">
                 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
@@ -143,7 +151,7 @@
                         <p class="services-more"><a href="#">Find Out More</a></p>
                     </div>
                 </div>
-
+        
                 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                     <div class="services-group wow animated zoomIn" data-wow-offset="40">
                         <p class="services-icon"><i class="fa fa-apple fa-5x"></i></p>
@@ -152,18 +160,18 @@
                         <p class="services-more"><a href="#">Find Out More</a></p>
                     </div>
                 </div>
-
+        
                 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                     <div class="services-group wow animated fadeInRight" data-wow-offset="40">
                         <p class="services-icon"><i class="fa fa-thumbs-o-up fa-5x"></i></p>
                         <h4 class="services-title">EASY TO USE</h4>
                         <p class="services-body">Cras interdum placerat libero vel tempor. Curabitur gravida iaculis erat quis dignissim.</p>
                         <p class="services-more"><a href="#">Find Out More</a></p>
-                    </div>
+                    </div>        
                 </div>
             </div>
             <!-- End Serivces Row 1 -->
-
+      
             <!-- Begin Services Row 2 -->
             <div class="row services-row services-row-tail services-row-2">
                 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
@@ -174,7 +182,7 @@
                         <p class="services-more"><a href="#">Find Out More</a></p>
                     </div>
                 </div>
-
+        
                 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                     <div class="services-group wow animated zoomIn" data-wow-offset="40">
                         <p class="services-icon"><i class="fa fa-eye fa-5x"></i></p>
@@ -183,7 +191,7 @@
                         <p class="services-more"><a href="#">Find Out More</a></p>
                     </div>
                 </div>
-
+        
                 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                     <div class="services-group wow animated fadeInRight" data-wow-offset="40">
                         <p class="services-icon"><i class="fa fa-cube fa-5x"></i></p>
@@ -194,20 +202,22 @@
                 </div>
             </div>
             <!-- End Serivces Row 2 -->
-
-        </div>
+    
+        </div>      
     </section>
     <!-- End #services-section -->
 
 
     <!-- Footer -->
-    <footer>
+    <footer> 
         <div class="container">
             <div class="row">
                 <div class="col-md-4">
                     <h3><i class="fa fa-map-marker"></i> Contact:</h3>
                     <p class="footer-contact">
-                        Via Ludovisi 39-45, Rome, 54267, Italy<br> Phone: 1.800.245.356 Fax: 1.800.245.357<br> Email: hello@LawOffice.org<br>
+                        Via Ludovisi 39-45, Rome, 54267, Italy<br>
+                        Phone: 1.800.245.356 Fax: 1.800.245.357<br>
+                        Email: hello@LawOffice.org<br>
                     </p>
                 </div>
                 <div class="col-md-4">
@@ -216,34 +226,37 @@
                     <p> <a href="#"> Services ( What we do )</a></p>
                     <p> <a href="#"> Contact ( Feel free to contact )</a></p>
                     <p> <a href="#"> Blog ( Write to us )</a></p>
-                    <p> <a href="#"> Team ( Meet the Team )</a></p>
+                    <p> <a href="#"> Team ( Meet the Team )</a></p> 
                 </div>
-                <div class="col-md-4">
-                    <h3><i class="fa fa-heart"></i> Socialize</h3>
-                    <div id="social-icons">
-                        <a href="#" class="btn-group google-plus">
+              <div class="col-md-4">
+                <h3><i class="fa fa-heart"></i> Socialize</h3>
+                <div id="social-icons">
+                    <a href="#" class="btn-group google-plus">
                         <i class="fa fa-google-plus"></i>
                     </a>
-                        <a href="#" class="btn-group linkedin">
+                      <a href="#" class="btn-group linkedin">
                         <i class="fa fa-linkedin-square"></i>
                     </a>
-                        <a href="#" class="btn-group twitter">
+                      <a href="#" class="btn-group twitter">
                         <i class="fa fa-twitter"></i>
                     </a>
-                        <a href="#" class="btn-group facebook">
+                      <a href="#" class="btn-group facebook">
                         <i class="fa fa-facebook"></i>
                     </a>
-                    </div>
                 </div>
-            </div>
+              </div>    
         </div>
+      </div>
     </footer>
 
-
+    
     <div class="copyright text center">
         <p>&copy; Copyright 2014, <a href="#">Your Website Link</a>. Theme by <a href="https://themewagon.com/" target="_blank">ThemeWagon</a></p>
     </div>
-    <?php wp_footer(); ?>
-</body>
 
+    
+    
+
+    <?php wp_footer(); ?>
+  </body>
 </html>
